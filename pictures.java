c5a4e92e2e91cc146e985314befbcfd8846f3320
michelle9919 <62105631+michelle9919@users.noreply.github.com>
2022-11-07 09:36:19 +0800

    Update pictures.java

--- pictures.java
+++ pictures.java
@@ -25,11 +25,11 @@ class MyJFrame extends JFrame implements ActionListener {
     contentPane.setLayout(new GridLayout(1, 2));
     add(contentPane);
 
-    btn1 = new JButton("previous");
+    btn1 = new JButton("Previous picture");
     btn1.addActionListener(this);
     contentPane.add(btn1);
 
-    btn2 = new JButton("next");
+    btn2 = new JButton("Next picture");
     btn2.addActionListener(this);
     contentPane.add(btn2);
 
@@ -38,14 +38,14 @@ class MyJFrame extends JFrame implements ActionListener {
 
   public void actionPerformed(ActionEvent e) {
     if (e.getSource() == btn1 && num == 0) {
-      JOptionPane.showMessageDialog(null, "There is no picture", "previous", JOptionPane.WARNING_MESSAGE);
+      JOptionPane.showMessageDialog(null, "There is no picture", "Warning message", JOptionPane.WARNING_MESSAGE);
     } else if (e.getSource() == btn1) {
       num--;
       label.setIcon(new ImageIcon(picturesName[num]));
     }
 
     if (e.getSource() == btn2 && num == picturesName.length - 1) {
-      JOptionPane.showMessageDialog(null, "There is no picture", "next", JOptionPane.WARNING_MESSAGE);
+      JOptionPane.showMessageDialog(null, "There is no picture", "Warning message", JOptionPane.WARNING_MESSAGE);
     } else if (e.getSource() == btn2) {
       num++;
       label.setIcon(new ImageIcon(picturesName[num]));
